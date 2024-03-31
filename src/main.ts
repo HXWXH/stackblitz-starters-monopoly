f0e27bd38e9f0cdf4f956a49bdd4b3ece2cc410a
HXWXH <zxc559512@gmail.com>
2024-03-31 13:04:50 +0800

    game-set-board send data compelet

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,14 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private messageSource = new BehaviorSubject<any>(null);
-  currentMessage = this.messageSource.asObservable();
+  // 勝利條件-資料訂閱
+  private winConditionSource = new BehaviorSubject<any>(null);
+  winConditionSelect = this.winConditionSource.asObservable();
 
-  setData(newData: any) {
-    this.messageSource.next(newData);
+  setWinCondition(newData: any) {
+    this.winConditionSource.next(newData);
+
+  //  XXX-資料訂閱
   }
 }
 
@@ -26,7 +29,11 @@ export class DataService {
   styleUrls: ['./template/css/gameSet.css']
 })
 export class gameSetApp {
-  message: any;
+  message: any; // message用於存取component傳遞的資料
+  selectWinCondition: any = {winCondition: 'getFirst', score: 'NaN'}; // 選擇勝利方式
+
+  // get element ID "gameSetBoard"
+  @ViewChild('gameSetBoard', {static: true}) gameSetBoard: ElementRef;
 
   // get button element ID "getFirst"
   @ViewChild('getFirst', {static: true}) getFirst: ElementRef;
@@ -34,13 +41,16 @@ export class gameSetApp {
   // get button element ID "getFirst"
   @ViewChild('getScore', {static: true}) getScore: ElementRef;
 
-  constructor(private dataService: DataService) {
-    this.dataService.currentMessage.subscribe(data => this.message = data);
-    // 初始化一个空的 ElementRef
-    this.getFirst = {} as ElementRef;
+  // get select element ID "scoreSelect"
+  @ViewChild('scoreSelect', {static: true}) scoreSelect: ElementRef;
 
+  constructor(private dataService: DataService) {
+    this.dataService.winConditionSelect.subscribe(data => this.message = data);
     // 初始化一个空的 ElementRef
+    this.gameSetBoard = {} as ElementRef;
+    this.getFirst = {} as ElementRef;
     this.getScore = {} as ElementRef;
+    this.scoreSelect = {} as ElementRef;
   }
 
   ngAfterViewInit() {
@@ -54,30 +64,63 @@ export class gameSetApp {
     const getFirstButton = this.getFirst.nativeElement;
     const getScoreButton = this.getScore.nativeElement;
 
+    // 顯示以及關閉選取框
     getFirstButton.classList.add('selectBorder');
     getScoreButton.classList.remove('selectBorder');
+
+    // 關閉分數選取
+    this.scoreDisabledTrue()
+
+    // 設置GetFirst給selectWinCondition
+    this.selectWinCondition.winCondition = getFirstButton.value;
+
+    // 設置目標分數
+    this.selectWinCondition.score = 'NaN';
   }
 
   selectGetScore() {
     // 獲取原生DOM元素
     const getScoreButton = this.getScore.nativeElement;
     const getFirstButton = this.getFirst.nativeElement;
+    const getScoreValue = this.scoreSelect.nativeElement;
 
+    // 顯示以及關閉選取框
     getScoreButton.classList.add('selectBorder');
     getFirstButton.classList.remove('selectBorder');
+
+    // 開啟分數選取
+    this.scoreDisabledFalse();
+
+    // 設置獲勝條件為達成目標分數
+    this.selectWinCondition.winCondition = getScoreButton.value;
+
+    // 設置目標分數
+    this.selectWinCondition.score = getScoreValue.value;
+    
   }
 
-  // select element togle
+  // 開啟關閉分數選取
   isDisabled: boolean = true;
 
-  disabledFalse() {
+  scoreDisabledFalse() {
     this.isDisabled = false;
   }
 
-  disabledTrue() {
+  scoreDisabledTrue() {
     this.isDisabled = true;
   }
-  test: string = 'test';
+
+  // 開始遊戲
+  startGameButton() {
+    // 獲取原生DOM元素
+    const getGameSetBoard = this.gameSetBoard.nativeElement;
+
+    // 隱藏gameSetBoard
+    getGameSetBoard.classList.add('invisible');
+
+    this.message = this.selectWinCondition;
+    this.dataService.setWinCondition(this.message);
+  }
 }
 
 
@@ -88,7 +131,19 @@ export class gameSetApp {
   templateUrl: './template/html/gameCellWork.html'
 })
 export class gameCellWorkApp {
-  
+  winCondition: any;
+
+  constructor(private dataService: DataService) {
+    this.dataService.winConditionSelect.subscribe(data => {
+      this.handleWinConditionSelect(data);
+    });
+  }
+
+   // winConditionSource資料更改時執行
+   handleWinConditionSelect(newData: string) {
+    this.winCondition = newData;
+  }
+
 }
 
 
