33e8a0f210062443f66cd98b8651fcea69a700c9
HXWXH <zxc559512@gmail.com>
2024-04-04 00:28:21 +0800

    20204/04/04 save . Can't display score ver3.5

--- src/main.ts
+++ src/main.ts
@@ -55,7 +55,7 @@ export class gameSetApp {
   // get button element ID "getFirst"
   @ViewChild('getFirst', {static: true}) getFirst: ElementRef;
 
-  // get button element ID "getFirst"
+  // get button element ID "getScore"
   @ViewChild('getScore', {static: true}) getScore: ElementRef;
 
   // get select element ID "scoreSelect"
@@ -131,37 +131,37 @@ export class gameSetApp {
   // 格子內容
   cellContentData=[
     {
-      class: 'question',
+      class: '問題',
       question: '如果小明有3支鉛筆，小華給了他5支鉛筆，請問小明現在總共有多少支鉛筆？',
       options: ['8支', '6支', '7支'],
       answer: '8支'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '如果一個蘋果賣5元，小美用20元買了幾個蘋果？',
       options: ['4個', '3個', '5個'],
       answer: '4個'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '如果一個長方形花園的長是6公尺，寬是3公尺，請問它的面積是多少平方公尺？',
       options: ['18平方公尺', '9平方公尺', '20平方公尺'],
       answer: '18平方公尺'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '如果一個數字比6大2，結果是多少？',
       options: ['8', '7', '9'],
       answer: '8'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '如果小明每天睡覺前都看書半小時，一個星期看了多少小時的書？',
       options: ['3.5小時', '3小時', '4.5小時'],
       answer: '3.5小時'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '「一鳴驚人」是用來形容什麼情況？',
       options: [
         '比喻平時默默無聞，而後卻突然有驚人的表現。',
@@ -170,7 +170,7 @@ export class gameSetApp {
       answer: '比喻平時默默無聞，而後卻突然有驚人的表現。'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '「畫蛇添足」這個成語的意思是什麼？',
       options: ['比喻多此一舉，反將事情弄糟。',
       '比喻平時默默無聞，而後卻突然有驚人的表現。',
@@ -178,7 +178,7 @@ export class gameSetApp {
       answer: '比喻多此一舉，反將事情弄糟。'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '「井底之蛙」這個成語是形容什麼樣的人？',
       options: ['比喻見識淺薄的人。',
       '比喻多此一舉，反將事情弄糟。',
@@ -186,7 +186,7 @@ export class gameSetApp {
       answer: '比喻見識淺薄的人。'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '「東施效顰」這個成語的意思是什麼？',
       options: ['比喻盲目胡亂地模仿他人，結果卻適得其反。',
       '比喻見識淺薄的人。',
@@ -194,7 +194,7 @@ export class gameSetApp {
       answer: '比喻盲目胡亂地模仿他人，結果卻適得其反。'
     },
     {
-      class: 'question',
+      class: '問題',
       question: '「十全十美」這個成語的意思是什麼？',
       options: ['比喻圓滿美好毫無缺陷的境界。',
       '比喻盲目胡亂地模仿他人，結果卻適得其反。',
@@ -202,27 +202,27 @@ export class gameSetApp {
       answer: '比喻圓滿美好毫無缺陷的境界。'
     },
     {
-      class: 'destiny',
+      class: '命運',
       content: '扶老奶奶過馬路，玩家前進3格',
       value: '3'
     },
     {
-      class: 'destiny',
+      class: '命運',
       content: '在警察面前闖紅燈，玩家退3格',
       value: '-3'
     },
     {
-      class: 'change',
+      class: '機會',
       content: '路上撿到500元，玩家加3分',
       value: '3'
     },
     {
-      class: 'change',
+      class: '機會',
       content: '踩到狗大便，玩家扣3分',
       value: '-3'
     },
     {
-      class: 'change',
+      class: '機會',
       content: '遇到外星人，玩家加5分',
       value: '5'
     }
@@ -273,7 +273,7 @@ export class gameSetApp {
   
     // 確認class是question
     isQuestion(cellContentData: any, element: any) {
-      if(cellContentData.class === 'question') {
+      if(cellContentData.class === '問題') {
         element.classList.add('questionCell');
         element.innerText = '問題';
       }
@@ -281,7 +281,7 @@ export class gameSetApp {
   
     // 確認class是change
     isChange(cellContentData: any, element: any) {
-      if(cellContentData.class === 'change') {
+      if(cellContentData.class === '機會') {
         element.classList.add('changeCell');
         element.innerText = '機會';
       }
@@ -289,7 +289,7 @@ export class gameSetApp {
   
     // 確認class是destiny
     isDestiny(cellContentData: any, element: any) {
-      if(cellContentData.class === 'destiny') {
+      if(cellContentData.class === '命運') {
         element.classList.add('destinyCell');
         element.innerText = '命運';
       }
@@ -309,12 +309,21 @@ export class gameCharacterApp {
   cellContent: any; // 格子內容儲存變數
   diceNum: any; // 儲存骰子的數值
   whoTurn: string = 'circle'; // 誰的回合
-  circlePosition:any ; // 圓形位子
-  squarePosition:any ; // 方形位子
+  circlePosition:any ; // 獲取圓形元素ID
+  squarePosition:any ; // 獲取方形元數ID
   winCondition: any; // 勝利條件儲存變數
   imageUrl: string; // 圖片Url
   run: any = 0; // 用於角色移動動畫
-  isButtonDisabled: boolean = false; // 按鈕 disable
+  isButtonDisabled: boolean = false; // 骰子按鈕 disable
+  AnswerButtonDisabled: boolean = false; // 選擇按鈕 disable
+  // 移動停止後，所觸發的事件
+  class: any;
+  question: any;
+  option1: any;
+  option2: any;
+  option3: any;
+  content: any;
+  value: any;
   // 玩家分數紀錄
   charactersPositionSoreLog: any = [
     {name: 'circle', currentPosition: 0, score: 0},
@@ -330,38 +339,55 @@ export class gameCharacterApp {
   // get element ID "dice"
   @ViewChild('dice', {static: true}) dice: ElementRef;
 
+  // get element ID "monopolyEvent"
+  @ViewChild('monopolyEvent', {static: true}) monopolyEvent: ElementRef;
+
+  // get element ID "monopolyBtn"
+  @ViewChild('monopolyBtn' , {static: true}) monopolyBtn: ElementRef;
+
+  // get element ID "btn1"
+  @ViewChild('btn1' , {static: true}) btn1: ElementRef;
+
+  // get element ID "btn2"
+  @ViewChild('btn2' , {static: true}) btn2: ElementRef;
+
+  // get element ID "btn3"
+  @ViewChild('btn3' , {static: true}) btn3: ElementRef;
+
+  // get element ID "monopolyContent"
+  @ViewChild('monopolyContent', {static: true}) monopolyContent: ElementRef;
+
+  // get element ID "checkAnswer"
+  @ViewChild('checkAnswer', {static: true}) checkAnswer: ElementRef;
+
   constructor(private dataService: DataService) {
     this.imageUrl = 'assets/roll.png';
     this.squareCharacter = {} as ElementRef;
     this.circleCharacter = {} as ElementRef;
     this.dice = {} as ElementRef;
-
+    this.monopolyEvent = {} as ElementRef;
+    this.monopolyBtn = {} as ElementRef;
+    this.monopolyContent = {} as ElementRef;
+    this.checkAnswer = {} as ElementRef;
+    this.btn1 = {} as ElementRef;
+    this.btn2 = {} as ElementRef;
+    this.btn3 = {} as ElementRef;
+  
     // 格子內容 問題、命運、機會
     this.dataService.cellContent.subscribe(data => this.cellContent = data);
-    // this.dataService.cellContent.subscribe(data => {
-    //   this.handle(data);
-    // });
-
-  }
-
-
-  handleCharacters(data:any) {
-    this.cellContent = data;
-    this.circleCharacter.nativeElement.style.left = 17 + 2*123 + 'px';
-    console.log('GAME CHARACTER',this.cellContent);
   }
 
   rollDice() {
     const randomInt = Math.floor(Math.random() * 6) + 1; // 隨機數值1~6
     this.diceNum = randomInt;
     this.imageUrl = 'assets/dice' + this.diceNum + '.png'; // 置入diceNum，用於圖片致換
-    this.isButtonDisabled = true; // disable 按鈕
+    this.isButtonDisabled = true; // 骰子 disable 按鈕
+    this.AnswerButtonDisabled = false; // 選擇 disable 按鈕
     
     this.run = 1;
 
     // 回合轉換
     this.whoTurn === 'circle' ? this.circleTurn(this.diceNum) : this.squareTurn(this.diceNum);
-
 
     // 測試
     // let circleCharacterPositiion =  this.circleCharacter.nativeElement.getBoundingClientRect();
@@ -398,10 +424,10 @@ export class gameCharacterApp {
     // }
     // 移動動畫
     if (this.run > this.diceNum) {
-      setTimeout(() => {
-      this.isButtonDisabled = false; // active 按鈕
-      this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
-      }, 500);
+      // setTimeout(() => {
+      // this.isButtonDisabled = false; // active 按鈕
+      // this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
+      // }, 500);
       return
     } else {
       setTimeout(() => {
@@ -420,10 +446,10 @@ export class gameCharacterApp {
   runLeft(character: any, diceNum: any, charactersPositionSoreLog: any) {
     // 移動動畫
     if (this.run > this.diceNum) {
-      setTimeout(() => {
-      this.isButtonDisabled = false; // active 按鈕
-      this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
-      }, 500);
+      // setTimeout(() => {
+      // this.isButtonDisabled = false; // active 按鈕
+      // this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
+      // }, 500);
       return
     } else {
       setTimeout(() => {
@@ -440,10 +466,10 @@ export class gameCharacterApp {
   runUp(character: any, diceNum: any, charactersPositionSoreLog: any) {
     // 移動動畫
     if (this.run > this.diceNum) {
-      setTimeout(() => {
-      this.isButtonDisabled = false; // active 按鈕
-      this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
-      }, 500);
+      // setTimeout(() => {
+      // this.isButtonDisabled = false; // active 按鈕
+      // this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
+      // }, 500);
       return
     } else {
       setTimeout(() => {
@@ -464,10 +490,10 @@ export class gameCharacterApp {
     // }
     // 移動動畫
     if (this.run > this.diceNum) {
-      setTimeout(() => {
-      this.isButtonDisabled = false; // active 按鈕
-      this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
-      }, 500);
+      // setTimeout(() => {
+      // this.isButtonDisabled = false; // active 按鈕
+      // this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
+      // }, 500);
       return
     } else {
       setTimeout(() => {
@@ -483,7 +509,6 @@ export class gameCharacterApp {
     }
   }
 
-  time: any = 1000;
   // log circle and square position
   cellPositionLog(character: any, diceNum: any, charactersPositionSoreLog: any) {
 
@@ -491,6 +516,48 @@ export class gameCharacterApp {
     if ((this.diceNum - this.run) !== -1) {
       charactersPositionSoreLog.currentPosition += 1;
     console.log('currentPosition:',charactersPositionSoreLog.currentPosition);
+    } else {
+
+  // class: any;
+  // question: any;
+  // option: any;
+  // content: any;
+  // value: any;
+
+      // 配置cell觸發事件
+      if (this.cellContent[charactersPositionSoreLog.currentPosition].class == "問題") {
+        this.monopolyContent.nativeElement.classList.add('displayNone');
+        this.monopolyBtn.nativeElement.classList.remove('displayNone');
+        this.class = this.cellContent[charactersPositionSoreLog.currentPosition].class;
+        this.question = this.cellContent[charactersPositionSoreLog.currentPosition].question;
+        this.option1 = this.cellContent[charactersPositionSoreLog.currentPosition].options[0];
+        this.option2 = this.cellContent[charactersPositionSoreLog.currentPosition].options[1];
+        this.option3 = this.cellContent[charactersPositionSoreLog.currentPosition].options[2];
+        this.content = '';
+        this.value = '';
+        this.endMove();
+      } else if((this.cellContent[charactersPositionSoreLog.currentPosition].class == "命運")||(this.cellContent[charactersPositionSoreLog.currentPosition].class == "機會")) {
+        this.monopolyBtn.nativeElement.classList.add('displayNone');
+        this.monopolyContent.nativeElement.classList.remove('displayNone');
+        this.class = this.cellContent[charactersPositionSoreLog.currentPosition].class;
+        this.question = '';
+        this.option1 = '';
+        this.option2 = '';
+        this.option3 = '';
+        this.content = this.cellContent[charactersPositionSoreLog.currentPosition].content;
+        this.value = this.cellContent[charactersPositionSoreLog.currentPosition].value;
+        this.closeMonopolyEventWindow(2500);
+        this.endMove();
+      } else {
+        setTimeout(() => {
+          this.isButtonDisabled = false; // active 按鈕
+        this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
+        },500);
+      }
+
+      
+
+      console.log('cellContent=',this.cellContent);
     }
 
    // 判斷位置
@@ -513,12 +580,48 @@ export class gameCharacterApp {
         charactersPositionSoreLog.currentPosition = 0;
       } 
     }
+  }
 
-    
+  answer(answer: any) {
+  
+    // 確認是誰的回合
+    let whoAnswerTurn:any ; 
+    this.whoTurn == 'square' ?
+    whoAnswerTurn = this.charactersPositionSoreLog[0]:
+    whoAnswerTurn = this.charactersPositionSoreLog[1] ;
+
+    this.AnswerButtonDisabled = true;
+    // whoAnswerTurn = this.cellContent[this.charactersPositionSoreLog[0].currentPosition] :
+    // whoAnswerTurn = this.cellContent[this.charactersPositionSoreLog[1].currentPosition] ;
+
+    if (answer == this.cellContent[whoAnswerTurn.currentPosition].answer) {
+      whoAnswerTurn.score += 5;
+      this.value = '非常棒~答案正確加 5 分';
+      this.closeMonopolyEventWindow(1300);
+    } else {
+      this.value = '答案錯誤X，多多加油唷~';
+      this.closeMonopolyEventWindow(1300);
+    }
     
   }
 
+  closeMonopolyEventWindow(delayTime: number) {
+    setTimeout(() => {
+      this.monopolyEvent.nativeElement.classList.add('invisible');
+    }, delayTime);
+  }
+
+  endMove() {
+    setTimeout(() => {
+      this.monopolyEvent.nativeElement.classList.remove('invisible');
+      this.isButtonDisabled = false; // active 按鈕
+      this.imageUrl = 'assets/roll.png'; // 置入roll，用於圖片致換
+    },500);
+  }
+
 }
+
+
 
 // Composite components
 @Component({
